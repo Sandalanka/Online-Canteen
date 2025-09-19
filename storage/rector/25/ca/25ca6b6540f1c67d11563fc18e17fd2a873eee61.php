590a5a8e9c469fba55e19a4fc8f6915baa95f313
Sandalanka <1415sandalanka@gmail.com>
2025-09-19 10:54:25 +0530

    code rector

---
+++ storage/rector/25/ca/25ca6b6540f1c67d11563fc18e17fd2a873eee61.php
@@ -0,0 +1,6 @@
+<?php declare(strict_types = 1);
+
+return \Rector\Caching\ValueObject\CacheItem::__set_state(array(
+   'variableKey' => 'file_hash',
+   'data' => '899a56396b49860f9c45110fe38a0cbb',
+));
